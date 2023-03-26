806ee1c0b0d0405e70e7f89a6d9df40d362d5a7a
salman <salmann9977@gmail.com>
2023-03-27 03:27:26 +0530

    connecting mongodb

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
+import connectDB from './mongodb/connect.js';
+
 dotenv.config();
 
 const app = express();
@@ -16,7 +18,7 @@ app.get('/', async (req, res) => {
 
 const startServer = async () => {
   try {
-    //   connectDB(process.env.MONGODB_URL);
+      connectDB(process.env.MONGODB_URL);
       app.listen(8080, () => console.log('Server started on port 8080'));
   } catch (error) {
       console.log(error);
