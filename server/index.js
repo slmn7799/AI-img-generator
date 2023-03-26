62195fcd6c25f11a4904baf5c1259395b53afffe
salman <salmann9977@gmail.com>
2023-03-27 03:51:39 +0530

    code for openai api integration in the backend

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js'
 
 dotenv.config();
 
